5174e26b42ed0d33158a8eec8c00c5110aaa3214
Comical <151361634+Comical78@users.noreply.github.com>
2025-02-07 21:07:08 +0200

    update

--- web/.vuepress/config.js
+++ web/.vuepress/config.js
@@ -1,7 +1,17 @@
+const vuepressBar = require("vuepress-bar");
+const { SideBar, NavBar } = require("./bars.js");
+const { sidebar } = vuepressBar({
+  addReadMeToFirstGroup: false,
+});
 module.exports = {
   title: "BlockmanLORD",
   description:
     "BlockmanLORD, Spargat's LORD Remake",
   base: "/",
   dest: "./dist",
+  themeConfig: {
+    navbar: NavBar,
+    sidebar: SideBar,
+    sidebarDepth: 2,
+  },
 };
