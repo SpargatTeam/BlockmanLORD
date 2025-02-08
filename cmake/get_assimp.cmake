350b3ea6d8f38b564a369a17a9202e471a16c747
Comical <151361634+Comical78@users.noreply.github.com>
2025-02-08 11:27:53 +0200

    Issuse - Windows Build, ZLIB

--- cmake/get_assimp.cmake
+++ cmake/get_assimp.cmake
@@ -6,6 +6,7 @@
 ##############################################################
 set(ASSIMP_DIR ${CMAKE_SOURCE_DIR}/libs/assimp)
 add_subdirectory(${ASSIMP_DIR} ${CMAKE_BINARY_DIR}/assimp_build)
+set(ASSIMP_BUILD_ZLIB OFF CACHE BOOL "Use system zlib" FORCE)
 target_include_directories(${PROJECT_NAME} PRIVATE
     ${ASSIMP_DIR}/include
     ${ASSIMP_DIR}/code
