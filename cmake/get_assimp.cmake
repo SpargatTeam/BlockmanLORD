5fed4a0de23763e8923abf9ebcc252ba8f0651dd
Comical <151361634+Comical78@users.noreply.github.com>
2025-02-08 11:35:24 +0200

    Issuse - Windows Build, ZLIB

--- cmake/get_assimp.cmake
+++ cmake/get_assimp.cmake
@@ -4,9 +4,10 @@
 # BlockmanLORD of Blockman Launcher CMake
 #
 ##############################################################
+set(ASSIMP_BUILD_ZLIB OFF CACHE BOOL "Use system zlib" FORCE)
+set(ASSIMP_NO_ZLIB ON CACHE BOOL "Disable internal zlib" FORCE)
 set(ASSIMP_DIR ${CMAKE_SOURCE_DIR}/libs/assimp)
 add_subdirectory(${ASSIMP_DIR} ${CMAKE_BINARY_DIR}/assimp_build)
-set(ASSIMP_BUILD_ZLIB OFF CACHE BOOL "Use system zlib" FORCE)
 target_include_directories(${PROJECT_NAME} PRIVATE
     ${ASSIMP_DIR}/include
     ${ASSIMP_DIR}/code
