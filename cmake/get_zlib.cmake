faa47760b7ad427a20d05ebc3123e23d1b4b876d
Comical <151361634+Comical78@users.noreply.github.com>
2025-02-08 16:06:00 +0200

    update

--- cmake/get_zlib.cmake
+++ cmake/get_zlib.cmake
@@ -4,7 +4,11 @@
 # BlockmanLORD of Blockman Launcher CMake
 #
 ##############################################################
-set(ZLIB_ROOT "${CMAKE_SOURCE_DIR}/libs/zlib")
-add_subdirectory(${ZLIB_ROOT} ${CMAKE_BINARY_DIR}/zlib_build)
-include_directories(${ZLIB_ROOT})
-target_link_libraries(${PROJECT_NAME} PRIVATE zlib)
+if(UNIX AND NOT APPLE)
+    set(ZLIB_ROOT "${CMAKE_SOURCE_DIR}/libs/zlib")
+    add_subdirectory(${ZLIB_ROOT} ${CMAKE_BINARY_DIR}/zlib_build)
+    include_directories(${ZLIB_ROOT})
+    target_link_libraries(${PROJECT_NAME} PRIVATE zlib)
+else()
+    message(STATUS "Zlib not needed.")
+endif()
