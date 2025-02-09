938e3fef4b97d68e1401499b39f3efd97ed0d78a
Comical <151361634+Comical78@users.noreply.github.com>
2025-02-09 19:25:38 +0200

    update

--- cmake/get_assimp.cmake
+++ cmake/get_assimp.cmake
@@ -5,9 +5,11 @@
 #
 ##############################################################
 set(ASSIMP_DIR ${CMAKE_SOURCE_DIR}/libs/assimp)
-add_subdirectory(${ASSIMP_DIR} ${CMAKE_BINARY_DIR}/assimp_build)
+set(ASSIMP_BINARY_DIR "${CMAKE_BINARY_DIR}/assimp_build")
+if(NOT TARGET assimp)
+    add_subdirectory(${ASSIMP_DIR} ${ASSIMP_BINARY_DIR})
+endif()
 target_include_directories(${PROJECT_NAME} PRIVATE
     ${ASSIMP_DIR}/include
-    ${ASSIMP_DIR}/code
 )
 target_link_libraries(${PROJECT_NAME} PRIVATE assimp)
